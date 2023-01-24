5053c4e3a22842ac2c70ccf6b83a966ff6d13b7f
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2023-01-24 12:07:37 -0600

    init infras

---
+++ src/index.js
@@ -0,0 +1,22 @@
+// Imports
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+
+// Import Utilities
+
+
+// Import Pages
+
+
+// Router
+const router = createBrowserRouter ([
+    {
+
+    }
+])
+
+// Version 18
+const app = document.getElementById("apps")
+const root = createRoot(app)
+root.render(<RouterProvider router={router} />)
